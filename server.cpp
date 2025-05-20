800f612795ea9319dd05c2faaf00f22819a20ebf
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-20 17:16:05 -0400

    console text setup

--- server.cpp
+++ server.cpp
@@ -25,4 +25,39 @@ struct Client
     thread th;
 };
 
+vector<Client> g_clients; // vector holding all connected clients
+mutex g_guard;            // used to control access to shared data across multiple threads -- ensures only one threat at a time can modify/cread g_guard
 
+void ansi_on() // enables vrt, enabling ansi colors in terminal
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;                                                   // variable to hold console mode flags
+    GetConsoleMode(h, &mode);                                     // stores current console mode flags in h
+    SetConsoleMode(h, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING); // add vrt
+};
+
+void say_console(string_view msg, bool nl = true)
+{
+    std::lock_guard lk(g_guard); // creates a lock on global mutex, ensuring exclusive access to console when message is printed
+    // prevents multiple threads from alternating between their ouput (messy)
+    std::cout << msg;
+    if (nl)
+        std::cout << "\n";
+}
+
+void broadcast_raw(const void *data, int bytes, int except_id = -1)
+{
+    std::lock_guard lk(g_guard);
+    for (auto &c : g_clients)
+    {
+        if (c.id != except_id)
+        {
+            send(c.sock, static_cast<const char *>(data), bytes, 0);
+        }
+    }
+}
+
+void broadcast_text(string_view text, int except_id = -1)
+{
+    broadcast_raw(text.data(), static_cast<int>(text.size()) + 1, except_id);
+}
