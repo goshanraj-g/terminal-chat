7bd8007aaf0bf6a02a54a71203c99fafd73a56aa
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-20 22:33:26 -0400

    client headers

---
+++ client.cpp
@@ -0,0 +1,12 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <string>
+#include <cstring>
+
+#include "common.hpp"
